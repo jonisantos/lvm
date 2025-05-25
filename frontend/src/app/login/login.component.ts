718810fd14d0f0091888568829dc397d66945d9b
Jonilson <jonnyzerobravo@gmail.com>
2025-05-25 11:16:08 -0400

    Criando a tela de login no front-end

---
+++ frontend/src/app/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterLink],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+
+  onSubmit() {
+    console.log('Login attempt:', this.email, this.password);
+    // Adicione lógica de autenticação aqui (ex: HTTP request)
+  }
+}
